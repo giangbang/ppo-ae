6973194b591e9b05ae27254dd73d961db54d379e
giangbang <banggiangle2015@gmail.com>
2022-11-15 12:30:24 +0700

    fix bug in setup.p

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
-from pip.req import parse_requirements
 from setuptools import setup, find_packages
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements(<requirements_path>)
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name = "ppo-ae",
     version = "0.0.1",
     description = (""),
     packages=find_packages(),
-    install_requires=reqs,
+    install_requires=[],
 )
